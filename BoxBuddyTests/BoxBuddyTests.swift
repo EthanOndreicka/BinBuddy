4365be535606ecf53d9f423b3ce051a93107cf96
Ethan <ethan.ondreicka1@marist.edu>
2025-10-02 18:48:55 -0400

    Initial Commit

---
+++ BoxBuddyTests/BoxBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoxBuddyTests.swift
+//  BoxBuddyTests
+//
+//  Created by Ethan Ondreicka on 10/2/25.
+//
+
+import Testing
+@testable import BoxBuddy
+
+struct BoxBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
